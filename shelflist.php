7f526ca9c4e08e4b76fa2812905c72b40fe01084
Dude <craig.boman@gmail.com>
2018-09-22 18:46:34 -0400

    revert

--- shelflist.php
+++ shelflist.php
@@ -119,74 +119,52 @@ $connection->query('SET NAMES UNICODE');
 
 $sql = '
 SELECT
--- i.inventory_gmt,
-lower(p.barcode) as barcode,
-upper(p.call_number_norm || COALESCE(' ' || v.field_content, '') ) as call_number_norm,
+
+-- p.call_number_norm,
+upper(p.call_number_norm) as call_number_norm,
+v.field_content as volume,
+i.location_code, i.item_status_code,
 b.best_title,
-i.location_code,
-i.item_status_code,
-s.content AS inventory_note,
-to_timestamp(c.due_gmt::text, 'YYYY-MM-DD HH24:MI:SS') as due_gmt --some dates may require 24 hour time stamp; idk
+c.due_gmt, i.inventory_gmt
+
+-- *
 
 FROM
-sierra_view.item_record_property	AS p
+
+sierra_view.phrase_entry				AS e
+
 JOIN
-sierra_view.item_record			AS i
+sierra_view.item_record_property		AS p
 ON
-  p.item_record_id = i.id
+  e.record_id = p.item_record_id
+
+  JOIN sierra_view.item_record			AS i
+ON
+  i.id = p.item_record_id
+
+LEFT OUTER JOIN sierra_view.checkout	AS c
+ON
+  i.id = c.item_record_id
+
+-- This JOIN will get the Title and Author from the bib
+JOIN
+sierra_view.bib_record_item_record_link	AS l
+ON
+  l.item_record_id = e.record_id
+JOIN
+sierra_view.bib_record_property			AS b
+ON
+  l.bib_record_id = b.bib_record_id
 
 LEFT OUTER JOIN
-sierra_view.subfield			AS s
+sierra_view.varfield					AS v
 ON
-  (s.record_id = p.item_record_id) AND s.field_type_code = 'w'
-
-LEFT OUTER JOIN
-sierra_view.checkout			AS c
-ON
-  (i.record_id = c.item_record_id)
-
-LEFT OUTER JOIN
-sierra_view.varfield			AS v
-ON
-  i.id = v.record_id AND v.varfield_type_code = 'v'
-
-LEFT JOIN
-sierra_view.bib_record_item_record_link AS l
-ON
-  l.item_record_id = i.id
-
-LEFT JOIN
-sierra_view.bib_record_property as b
-ON
-  b.bib_record_id = l.bib_record_id
+  (i.id = v.record_id) AND (v.varfield_type_code = \'v\')
 
 WHERE
-i.location_code = \'' . $barcode . '\'
---   --comment out this section for items organized by title
--- AND
--- p.call_number_norm >= lower('BF   77 U53 1992 SE')
--- AND
--- p.call_number_norm <= lower('TX  335 L69 1990 TR BOOK')
-
-
--- since we have the situation where multiple bibs can share the same item record, we should remove duplicated items.
--- we need to tweak this ... not sure grouping is the best way to do this.
-
--- group by
--- s.content,
--- c.due_gmt,
--- p.barcode, p.call_number_norm,
--- v.field_content,
--- l.items_display_order,
--- i.location_code, i.item_status_code
-
-order by
---b.best_title ASC, --for periodicals which are sorted by title
-p.call_number_norm ASC,
-l.items_display_order ASC
-
---LIMIT 10000
-
+e.index_tag || e.index_entry = \'b\' || UPPER(\'' . $barcode . '\')
+OR
+e.index_tag || e.index_entry = \'b\' || LOWER(\'' . $barcode . '\')
 ';
 
 $statement = $connection->prepare($sql);
