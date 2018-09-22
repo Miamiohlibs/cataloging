c806b75a20a2b7169615db3c0f35671eb16d8dd8
Dude <craig.boman@gmail.com>
2018-09-22 18:06:54 -0400

    sanity test

--- shelflist.php
+++ shelflist.php
@@ -1,21 +1,81 @@
 <?php
 //https://raw.githubusercontent.com/rayvoelker/2015RoeschLibraryInventory/master/php/inventory_barcode.php
+function left_pad_number($number, $pad_amount) {
+	//returns a string value of a number padded out to the maximum length of the $pad_amount
+
+	//if the length of the number is the same or greater than the pad_amount, just return the number unpadded
+	if ( strlen($number) >= $pad_amount ) {
+		return $number;
+	}
+
+	$result = array();
+	$number = array_map('intval', str_split($number));
+
+	//pop off values from the end of number and push them onto the $result stack
+	while ($number) {
+		array_push($result, array_pop($number) );
+	}
+
+	while ( count($result) < $pad_amount ) {
+		array_push($result, " ");
+	}
+
+	$result = array_reverse($result);
+	$string = implode('', $result);
+
+	return $string;
+}
+
+function normalize_volume($string_data) {
+	//will return a string formatted to sort properly among other volumes
+	// For example:
+	// given a volume number:
+	// "v.1"
+	// will return:
+	// "v.    1"
+
+	// given a volume number:
+	// "v.11"
+	// will return:
+	// "v.   11"
+
+	$return_string = "";
+	$len = strlen($string_data);
+
+	//split everything that is a number, and everything that is not a number into $matches
+	$regex = "/[0-9]+|[^0-9]+/";
+	preg_match_all($regex, $string_data, $matches);
+
+	for($i=0; $i<count($matches[0]); $i++) {
+		if ( is_numeric ($matches[0][$i]) ) {
+			$matches[0][$i] = left_pad_number($matches[0][$i], 5);
+		}
+	}
+
+	$string = implode('', $matches[0]);
+
+	return $string;
+} //end function normalize_callnumber
 
 
 // sanitize the input
-if ( isset($_GET['location']) )  {
+if ( isset($_GET['barcode']) )  {
 	header("Content-Type: application/json");
-
-
+	// ensure that the barcode value is formatted somewhat sanely
+	if( strlen($_GET['barcode']) > 14 ) {
+		//we don't expect barcodes to be longer than 12 alpha-numeric characters
+		//although, 99.9 % of our scannable barcodes are 10 digit, I'm leaving some breathing room
+		echo "{}";
+		die();
+	}
 	// barcodes are ONLY alpha-numeric ... strip anything that isn't this.
-	$location = preg_replace("/[^a-zA-Z0-9\s]/", "", $_GET['location']);
-	echo $location;
+	$barcode = preg_replace("/[^a-zA-Z0-9\s]/", "", $_GET['barcode']);
 }
 else{
 	die();
 }
 
-/* // commenting out for testing
+/*
 
 include file (item_barcode.php) supplies the following
 arguments as the example below illustrates :
@@ -29,7 +89,6 @@ arguments as the example below illustrates :
 		. "sslmode=require;"
 		. "charset=utf8;"
 */
-
 
 //reset all variables needed for our connection
 $username = null;
@@ -60,74 +119,52 @@ $connection->query('SET NAMES UNICODE');
 
 $sql = '
 SELECT
--- i.inventory_gmt,
-lower(p.barcode) as barcode,
-upper(p.call_number_norm || COALESCE(' ' || v.field_content, '') ) as call_number_norm,
+
+-- p.call_number_norm,
+upper(p.call_number_norm) as call_number_norm,
+v.field_content as volume,
+i.location_code, i.item_status_code,
 b.best_title,
-i.location_code,
-i.item_status_code,
-s.content AS inventory_note,
-to_timestamp(c.due_gmt::text, 'YYYY-MM-DD HH24:MI:SS') as due_gmt --some dates may require 24 hour time stamp; idk
+c.due_gmt, i.inventory_gmt
+
+-- *
 
 FROM
-sierra_view.item_record_property	AS p
+
+sierra_view.phrase_entry				AS e
+
 JOIN
-sierra_view.item_record			AS i
+sierra_view.item_record_property		AS p
 ON
-  p.item_record_id = i.id
+  e.record_id = p.item_record_id
+
+  JOIN sierra_view.item_record			AS i
+ON
+  i.id = p.item_record_id
+
+LEFT OUTER JOIN sierra_view.checkout	AS c
+ON
+  i.id = c.item_record_id
+
+-- This JOIN will get the Title and Author from the bib
+JOIN
+sierra_view.bib_record_item_record_link	AS l
+ON
+  l.item_record_id = e.record_id
+JOIN
+sierra_view.bib_record_property			AS b
+ON
+  l.bib_record_id = b.bib_record_id
 
 LEFT OUTER JOIN
-sierra_view.subfield			AS s
+sierra_view.varfield					AS v
 ON
-  (s.record_id = p.item_record_id) AND s.field_type_code = 'w'
-
-LEFT OUTER JOIN
-sierra_view.checkout			AS c
-ON
-  (i.record_id = c.item_record_id)
-
-LEFT OUTER JOIN
-sierra_view.varfield			AS v
-ON
-  i.id = v.record_id AND v.varfield_type_code = 'v'
-
-LEFT JOIN
-sierra_view.bib_record_item_record_link AS l
-ON
-  l.item_record_id = i.id
-
-LEFT JOIN
-sierra_view.bib_record_property as b
-ON
-  b.bib_record_id = l.bib_record_id
+  (i.id = v.record_id) AND (v.varfield_type_code = \'v\')
 
 WHERE
-i.location_code = '$location'
---   --comment out this section for items organized by title
--- AND
--- p.call_number_norm >= lower('PR 4879 L2 D83 2009')
--- AND
--- p.call_number_norm <= lower('PZ    7 B8163 WH19')
-
-
--- since we have the situation where multiple bibs can share the same item record, we should remove duplicated items.
--- we need to tweak this ... not sure grouping is the best way to do this.
-
--- group by
--- s.content,
--- c.due_gmt,
--- p.barcode, p.call_number_norm,
--- v.field_content,
--- l.items_display_order,
--- i.location_code, i.item_status_code
-
-order by
---b.best_title ASC, --for periodicals which are sorted by title
-p.call_number_norm ASC,
-l.items_display_order ASC
-
---LIMIT 10000
-
+e.index_tag || e.index_entry = \'b\' || UPPER(\'' . $barcode . '\')
+OR
+e.index_tag || e.index_entry = \'b\' || LOWER(\'' . $barcode . '\')
 ';
 
 $statement = $connection->prepare($sql);
@@ -146,7 +183,4 @@ echo json_encode($row);
 $row = null;
 $statement = null;
 $connection = null;
-
- //remove after testing
-
 ?>
